119f05c6d314d440f08c4267025f98c58b66f019
Aftab <aftab2060@gmail.com>
2023-10-16 20:09:22 +0600

    modified

--- products.js
+++ products.js
@@ -1,8 +1,14 @@
-function ProductsAPI(displayData) {
-	fetch("https://aftabwebdev.github.io/api/products/products.json")
-		.then((res) => res.json())
-		.then((data) => displayData(data))
-		.catch((err) => err.message);
+async function ProductsAPI(displayData) {
+	const response = await fetch(
+		"https://aftabwebdev.github.io/api/products/products.json"
+	);
+	const data = await response.json();
+	displayData(data);
+
+	// fetch("https://aftabwebdev.github.io/api/products/products.json")
+	// 	.then((res) => res.json())
+	// 	.then((data) => displayData(data))
+	// 	.catch((err) => err.message);
 }
 
 export default ProductsAPI;
