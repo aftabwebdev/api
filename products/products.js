96ee0fdd258242d5bba11d5a09a154462eccf80d
Aftab <aftab2060@gmail.com>
2023-10-16 15:40:22 +0600

    file added

---
+++ products/products.js
@@ -0,0 +1,6 @@
+function fetchAPI(displayData) {
+	fetch("https://aftabwebdev.github.io/api/products/products.json")
+		.then((res) => res.json())
+		.then((data) => displayData(data))
+		.catch((err) => err.message);
+}
